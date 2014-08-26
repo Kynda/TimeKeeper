ff8980ad37330b2c001bbae877c125287e4b28f3
Joseph Hallenbeck <joseph@iounn.local>
2014-08-26 17:57:00 -0500

    Added config example

---
+++ config.example.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  Config
+ * 
+ *  Defines configuration values.
+ *   
+ *  @package genus
+ *  @author Joe Hallenbeck
+ *
+ */
+
+$config = array();
+
+$config['db'] = array(
+                    'driver'     => 'pdo_mysql',
+                    'dbname'     => 'timekeeper',
+                    'user'       => '',
+                    'password'   => '',       
+               );
