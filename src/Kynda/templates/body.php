95086662a15eccb335dd4a8af19c6c9426e6a0ff
Joseph Hallenbeck <joseph@iounn.local>
2014-08-28 16:18:27 -0500

    Working on alerts

--- src/Kynda/templates/body.php
+++ src/Kynda/templates/body.php
@@ -6,15 +6,14 @@
 
     </div>
 
+    <div class="alert alert-danger fade in" role="alert" id="del-alert">
+        <button type="button" class="close" id="del_cancel">
+                <span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
+        </button>
 
-    <div class="alert alert-block alert-error hide" id="del-alert">
-        <button type="button" class="close" id="close_delete">x</button>
-        <h4 class="alert-heading">Are you sure you want to delete this item?</h4>
-        <table class="table table-condensed task-table">
-            <tr id="delete_row"></tr>
-        </table>
+        <h4>Are you sure you want to delete this item?</h4>
         <p>
-        <a class="btn btn-danger" id="delete_btn" href="#">Delete</a>
+            <a class="btn btn-danger" id="delete_btn" href="#">Delete</a>
         </p>
     </div>
 
